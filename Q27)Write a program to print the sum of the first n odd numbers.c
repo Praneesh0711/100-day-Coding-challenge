1cace761a101e24cf54bd96e0c64ba53023b304f
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2025-09-21 23:08:20 +0530

    Create Q27)Write a program to print the sum of the first n odd numbers.c

---
+++ Q27)Write a program to print the sum of the first n odd numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the number till which you want the sum of the odd numbers: ");
+  scanf("%d", &n);
+
+  int sum = n * n;
+  printf("%d\n", sum);
+
+  return 0;
+}
