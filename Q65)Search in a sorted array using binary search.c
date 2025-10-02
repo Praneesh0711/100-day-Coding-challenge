f89324df8d4440e0d2d4141343182ba8195d7ed6
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2025-10-02 12:44:27 +0530

    Create Q65)Search in a sorted array using binary search.c

---
+++ Q65)Search in a sorted array using binary search.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, arr[100], key;
+    int low, high, mid, found = -1;
+
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            found = mid;
+            break;
+        }
+        else if(arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if(found != -1)
+    {
+        printf("Found at index %d\n", found);
+    }
+    else
+    {
+        printf("-1\n");
+    }
+
+    return 0;
+}
