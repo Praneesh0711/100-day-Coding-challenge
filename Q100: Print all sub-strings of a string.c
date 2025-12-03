57f2f864f99a7865af62e2399acf51a65d05ae90
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2025-12-03 10:58:34 +0530

    Create Q100: Print all sub-strings of a string.c

---
+++ Q100: Print all sub-strings of a string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j, len;
+
+    scanf("%s", str);
+    len = strlen(str);
+
+    for(i = 0; i < len; i++) {
+        for(j = i; j < len; j++) {
+            for(int k = i; k <= j; k++) {
+                printf("%c", str[k]);
+            }
+            if(!(i == len-1 && j == len-1)) {
+                printf(",");
+            }
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
