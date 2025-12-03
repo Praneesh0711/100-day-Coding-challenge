5d20499ce860c3f1de984602aaf097b534563390
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2025-12-03 11:13:08 +0530

    Create Q119 Day 69.c
    Implement a program to detect the first repeated element in an array using a frequency array.

---
+++ Q119 Day 69.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Using a simple frequency array to detect repetition in one pass
+    int freq[n+1]; 
+    for(int i = 0; i <= n; i++) {
+        freq[i] = 0;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(freq[arr[i]] == 1) {
+            printf("%d\n", arr[i]);
+            return 0;
+        }
+        freq[arr[i]] = 1;
+    }
+
+    return 0;
+}
