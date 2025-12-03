981b8814eea6a26b6ed67cb5074e4e7bd50e35fe
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2025-12-03 11:16:09 +0530

    Update Q123 Day 73.c

--- Q123 Day 73.c
+++ Q123 Day 73.c
@@ -1,1 +1,46 @@
 
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    fp = fopen("sample.txt", "r");
+    if(fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    while((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        if(ch == '\n') {
+            lines++;
+        }
+
+        if(isspace(ch)) {
+            inWord = 0;
+        } else {
+            if(inWord == 0) {
+                words++;
+                inWord = 1;
+            }
+        }
+    }
+
+    fclose(fp);
+
+    // If file has content but no newline, count last line
+    if(characters > 0 && ch == EOF) {
+        lines++;
+    }
+
+    printf("Characters: %d\n", characters);
+    printf("Words: %d\n", words);
+    printf("Lines: %d\n", lines);
+
+    return 0;
+}
