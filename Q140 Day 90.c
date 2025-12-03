2884fe28360a0f9018b912e78959928dcd9162aa
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2025-12-03 11:23:44 +0530

    Create Q140 Day 90.c

---
+++ Q140 Day 90.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Gender { MALE, FEMALE, OTHER };
+
+struct Person {
+    char name[50];
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+    char input[20];
+
+    printf("Enter gender (MALE/FEMALE/OTHER): ");
+    scanf("%s", input);
+
+    if(strcmp(input, "MALE") == 0) {
+        p.gender = MALE;
+    } else if(strcmp(input, "FEMALE") == 0) {
+        p.gender = FEMALE;
+    } else if(strcmp(input, "OTHER") == 0) {
+        p.gender = OTHER;
+    } else {
+        printf("Invalid gender!\n");
+        return 1;
+    }
+
+    switch(p.gender) {
+        case MALE:
+            printf("Male\n");
+            break;
+        case FEMALE:
+            printf("Female\n");
+            break;
+        case OTHER:
+            printf("Other\n");
+            break;
+        default:
+            printf("Unknown\n");
+    }
+
+    return 0;
+}
